44bd1cb151e4b81008fa41f87b4e5d63ceb3cfd8
RICARDO210394 <bohorquez-ricardo9628@unesum.edu.ec>
2026-08-21 10:45:07 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "bunker-rondas-v" + 31;
-const BUILD_TS = "2026-08-21T08-30-00";
+const CACHE_NAME = "bunker-rondas-v" + 32;
+const BUILD_TS = "2026-08-21T08-45-00";
 
 self.addEventListener("install", (event)=>{
   self.skipWaiting();
